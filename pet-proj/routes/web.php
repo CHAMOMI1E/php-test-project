de0bad10e74fd96cba0677afb902e66cfcadf861
CHAMOMILE <CHAMOM2LES@outlook.com>
2023-10-12 23:48:49 +0300

    first commit

---
+++ pet-proj/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function ()
+{
+    return view('about');
+});
+
+Route::get('/user/{id}/{name}', function ($id, $name)
+{
+    return "$id - $name";
+});
