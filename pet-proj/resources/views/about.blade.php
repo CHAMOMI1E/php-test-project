81db3de1b4968ebc55eb55808d590bf4792f3d14
CHAMOMILE <CHAMOM2LES@outlook.com>
2023-10-14 01:31:42 +0300

    add read.blade.php

--- pet-proj/resources/views/about.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('base')
-
-@section('main_content')
-    <h1>About</h1>
-@endsection
---
+++ pet-proj/resources/views/read.blade.php
@@ -0,0 +1,11 @@
+@extends('base')
+@section('title_content')
+    Чтение
+@endsection
+@section('main_content')
+    <div class="container mx-auto my-5 text-lg " data-bs-theme="dark">
+
+        <h1>About</h1>
+<p>smth</p>
+    </div>
+@endsection
